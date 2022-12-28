fb7162c89e93777c8003a4c5b151f3fed8c1cbaa
BeatrizBL <beatriz.bueno.larraz@gmail.com>
2022-12-28 16:58:23 +0100

    Template day

---
+++ X_Template/README.md
@@ -0,0 +1,16 @@
+# Day X: Title
+## Part One
+
+Description.
+
+**Question**
+
+## Part Two
+
+Description.
+
+**Question**
+
+# Solution used
+
+Explanation.
---
+++ X_Template/template.py
@@ -0,0 +1,12 @@
+from helpers import read_file_into_list
+
+def method() -> object:
+    pass
+
+if __name__ == '__main__':
+    input = read_file_into_list(path='X_Template/X_input.txt')
+    answer = None
+    print(f'Answer to part 1: {answer}')
+
+    answer = None
+    print(f'Answer to part 2: {answer}')
